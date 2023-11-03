403d2b1b9939094b9875ce1e3b2ebac2b19d370e
H-Ramalan <talktoramalan@gmail.com>
2023-11-03 01:04:48 -0700

    routes modification

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
-  # root 'splash#index'
-  root 'categories#index'
+  authenticated :user do
+    root 'categories#index', as: :authenticated_root
+  end
+  root 'splash#index'
 
-  # resources :transaction_categories
   resources :categories do
     resources :transactions
   end
   resources :transactions
-  resources :users
+  # resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
